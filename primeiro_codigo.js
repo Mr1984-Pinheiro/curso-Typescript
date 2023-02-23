e1f3478f2dd1b0368d1faa056eef4da17bd9560a
CARLOS EDUARDO TAVARES PINHEIRO <73333067+Mr1984-Pinheiro@users.noreply.github.com>
2023-02-23 09:47:18 -0300

    Arquivo de configuração

--- primeiro_codigo.js
+++ primeiro_codigo.js
@@ -1,4 +1,18 @@
-console.log("Hello World! Este é o meu primeiro códeigo TypeScript ");
+console.log("Hello World! Este é o meu primeiro código TypeScript ");
 console.log("========================================");
-var nome = "Carlos";
+//Cannot redeclare block-scoped variable 'nome'. Mesmo arquivo dá esse erro
+const nome = "Carlos";
+//tranforma em var - EcmaScript 3 para navegadores e sistemas mais antigos 
 console.log("Olá" + nome + ". Seja bem-vindo!");
+//tsc primeiro_codigo.ts --help fornece as flags para versoes de EcmaScript
+/*-t VERSION, --target VERSION
+Specify ECMAScript target version: 'ES3' (default), 'ES5', 'ES2015',
+'ES2016', 'ES2017', 'ES2018', 'ES2019', 'ES2020', or 'ESNEXT'. */
+console.log("========================================");
+class Produto {
+    constructor(produtoNome, produtoValor) {
+        this.nome = produtoNome;
+        this.valor = produtoValor;
+    }
+}
+const playstation5 = new Produto("Playstation5", 5000);
