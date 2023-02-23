5011befa59ba368153adfc111dd47b588836ca3f
CARLOS EDUARDO TAVARES PINHEIRO <73333067+Mr1984-Pinheiro@users.noreply.github.com>
2023-02-23 08:54:31 -0300

    compilador TSC

---
+++ primeiro_codigo.js
@@ -0,0 +1,4 @@
+console.log("Hello World! Este é o meu primeiro códeigo TypeScript ");
+console.log("========================================");
+var nome = "Carlos";
+console.log("Olá" + nome + ". Seja bem-vindo!");
---
+++ primeiro_codigo.ts
@@ -0,0 +1,7 @@
+console.log("Hello World! Este é o meu primeiro códeigo TypeScript ");
+
+console.log("========================================");
+
+const nome = "Carlos";
+
+console.log("Olá" + nome + ". Seja bem-vindo!");
